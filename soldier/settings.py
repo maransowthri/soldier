9e3ca7b3c84c4fd79420847ec30dc5882e75a438
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-12-30 14:31:28 +0530

    Removing secret keys

--- soldier/settings.py
+++ soldier/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$l#z7i7f@ncqo+%cz)opk^2osl9x^q&*)ep2#r&i6g3k1&x*y6'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
